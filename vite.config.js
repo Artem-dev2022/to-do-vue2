15f757169011f39fa4e513b154f3318f0feb87d1
Artem <artem-agayan@mail.ru>
2023-06-19 22:14:34 +0300

    changed base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/to-do-vue2',
+  base: '/to-do-vue2/',
   plugins: [
     vue2(),
     legacy({
