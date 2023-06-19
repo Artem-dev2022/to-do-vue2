6ca86ae0e4c885e1086740e1edad9caf7a18e751
Artem <artem-agayan@mail.ru>
2023-06-19 11:23:55 +0300

    build

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,10 @@ const router = new VueRouter({
       path: '/tasks',
       name: 'tasks',
       component: TasksView
+    },
+    {
+      path: '*',
+      component: TasksView
     }
   ]
 })
